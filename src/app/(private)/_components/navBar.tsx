c77db95a3b43c64bb63ee603b113dc1d9bac7e07
Henrique Becker <henribecker18@gmail.com>
2025-08-24 15:55:56 -0300

    Mudança no Menu navBar

--- src/app/(private)/_components/navBar.tsx
+++ src/app/(private)/_components/navBar.tsx
@@ -1,17 +1,59 @@
-import { Box, Container, Image, Link as ChakraLink } from "@chakra-ui/react";
+import { Box, Container, Image, Link as ChakraLink, Menu, Button, Portal } from "@chakra-ui/react";
 import Link from "next/link";
 
-type linkprops = {
+type SubLink = {
   nome: string
   url: string
 }
 
-const LinkURL: linkprops[] = [
+type LinkProps ={
+  nome: string
+  subLinks: SubLink[]
+}
+
+const LinkURL: SubLink[] = [
   {nome: 'Produtos', url: '/produtos'},
   {nome: 'Grupos / Categorias', url: '/grupos'},
   {nome: 'Iventários', url: '/inventarios'},
   {nome: 'Compras', url: '/compras'},
   {nome: 'Usuário / Perfil', url: '/perfil'},
+]
+
+const MenuLinks: LinkProps[] = [
+  {
+    nome: 'Estoque',
+    subLinks: [
+      {nome: 'Produtos', url: '/produtos'},
+      {nome: 'Compras', url: '/compras'},
+      {nome: 'Categorias e Grupo', url: '/grupos'},
+      {nome: 'Invetário', url: '/inventario'},
+    ]
+  },
+  {
+    nome: 'Financeiro',
+    subLinks: [
+      {nome: 'Vendas', url: '/recebiveis'},
+      {nome: 'A Pagar', url: '/duplicatas'},
+      {nome: 'Relatórios', url: '/relatorios'},
+      {nome: 'Fluxo de Caixa', url: '/fluxo-caixa'},
+      {nome: 'Bancos', url: '/bancos'},
+    ]
+  },
+  {
+    nome: 'Usuário',
+    subLinks: [
+      {nome: 'Perfil', url: '/perfil'},
+      
+    ]
+  },
+  {
+    nome: 'Controle de Produção',
+    subLinks: [
+      {nome: 'Perfil', url: '/perfil'},
+      
+    ]
+  },
+
 ]
 
 
@@ -20,10 +62,28 @@ export function NavBarComponent(){
     <Container display={'flex'} fluid p={1}  justifyContent={'center'} flexWrap={'wrap'} borderBottom={'2px solid'} borderBottomColor={'orange.solid'}>
       <Image src={'/logo-laranja.png'} w={'200px'}/>
       <Box w={'100%'} p={2} display={'flex'} justifyContent={'space-evenly'}>
-        {LinkURL.map((item, index)=>(
-          <ChakraLink key={index} href={item.url} p={2} _hover={{bg:'orange.solid'}} outline={'none'} textDecoration={'none'}>
-            {item.nome}
-          </ChakraLink>
+        <ChakraLink p={'0 10px'} href='/' textDecoration={'none'} cursor={'pointer'} focusRing={'none'}
+          colorPalette={'orange'} color={'orange.solid'} _hover={{bgColor: 'orange.emphasized'}}
+        >Início</ChakraLink>
+        {MenuLinks.map((link)=>(
+          <Menu.Root>
+            <Menu.Trigger asChild>
+              <Button variant={'plain'} colorPalette={'orange'} color={'orange.solid'} focusRing={'none'} _hover={{bgColor: 'orange.emphasized'}}>
+                {link.nome}
+              </Button>
+            </Menu.Trigger>
+            <Portal >
+              <Menu.Positioner>
+                <Menu.Content colorPalette={'orange'}>
+                  {link.subLinks.map((subItem)=>(
+                    <Menu.Item value={subItem.nome} asChild >
+                      <ChakraLink  href={subItem.url} textDecoration={'none'} color={'orange.solid'} cursor={'pointer'}>{subItem.nome}</ChakraLink>
+                    </Menu.Item>
+                  ))}
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu.Root>
         ))}
       </Box>
     </Container>
